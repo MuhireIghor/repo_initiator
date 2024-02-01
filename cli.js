47284ed673d4eabfd498c38df2221cab046d431b
MuhireIghor <muhireighor123@gmail.com>
2024-02-01 18:55:21 +0200

    [fix]:Updated the script to allow bash execution

--- cli.js
+++ cli.js
@@ -1,23 +1,57 @@
-#!/bin/env node
+#!/usr/bin/env node
 
-'use strict'
+
 import { exec } from "child_process"
-import boxen from "boxen"
-import chalk from "chalk"
-const text = chalk.blue.bold("Made with love by <MuhireIghor />\n\n ")
-function main() {
-    exec("./index.sh", function (err, stdout, stderr) {
-        if (err) {
-            console.log(err);
-            return;
+import inquirer from "inquirer";
+import boxen from "boxen";
 
-        }
-        else if (stderr) {
-            console.log(stderr);
+
+async function main() {
+    const answers = await inquirer.prompt([
+        {
+            type: 'input',
+            name: 'githubUsername',
+            message: 'Enter your GitHub username:',
+        },
+        {
+            type: 'password',
+            name: 'githubPersonalAccessToken',
+            message: 'Enter your GitHub personal access token:',
+        },
+        {
+            type: 'input',
+            name: 'repositoryName',
+            message: 'Enter your repository name:',
+        },
+        {
+            type: 'input',
+            name: 'projectDirectory',
+            message: 'Enter your project directory:',
+        },
+    ]);
+
+    const { githubUsername, githubPersonalAccessToken, repositoryName, projectDirectory } = answers;
+
+    const command = `./index.sh "${githubUsername}" "${githubPersonalAccessToken}" "${repositoryName}" "${projectDirectory}"`;
+
+    exec(command, function (err, stdout, stderr) {
+        if (err) {
+            console.error('Error executing shell script:', err);
+            return;
+        } else if (stderr) {
+            console.error('Shell script errors:', stderr);
             return;
         }
-        console.log(boxen(text, { padding: 1, borderStyle: 'singleDouble', borderColor: 'blue', margin: '1', backgroundColor: 'black', height: 8 }))
-    })
+        console.log(stdout);
+        console.log(boxen('Made with love by <MuhireIghor />', {
+            margin: 1,
+            float: 'left',
+            padding: 1,
+            borderStyle: "single",
+            borderColor: "green"
+        }))
+
+    });
 }
 
-main()
+main();
