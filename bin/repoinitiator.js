d16726272578f3efe6ab854dd770b6285c136722
MURANGWA Pacifique <pacifiquemurangwa001@gmail.com>
2024-02-03 08:31:11 +0200

    feat: config command handling

--- bin/repoinitiator.js
+++ bin/repoinitiator.js
@@ -1,64 +1,68 @@
 #!/usr/bin/env node
 
-
-import { exec } from "child_process"
-import inquirer from "inquirer";
+import { exec } from "child_process";
 import boxen from "boxen";
+import inquirer from "inquirer";
+import { program } from "commander";
+import pkg from "../package.json" assert { type: "json" };
+import { writeFile, existsSync } from "fs";
+import chalk from "chalk";
+import { logMessage, logError } from "../lib/log.js";
+import { getConfigDir } from "../lib/config_dir.js";
 
+program
+  .name("repoinitiator")
+  .description("With CLI, Create a new repository on Github")
+  .usage("command [arguments]")
+  .version(
+    `\x1b[1mv${pkg.version}\x1b[0m`,
+    "-v, --version",
+    "Output repoinitiator's current version."
+  )
+  .helpOption("-h, --help", "Output usage of repoinitiator.");
 
-async function main() {
-    const answers = await inquirer.prompt([
+program
+  .command("config")
+  .description("Configure repoinitiator locally")
+  .action(async () => {
+    const config_dir = getConfigDir();
+    if (existsSync(config_dir)) {
+      logMessage(chalk.redBright.bold("Configurations already exists"));
+      return;
+    } else {
+      const answers = await inquirer.prompt([
         {
-            type: 'input',
-            name: 'githubUsername',
-            message: 'Enter your GitHub username:',
+          type: "input",
+          name: "githubUsername",
+          message: "GitHub username: ",
         },
         {
-            type: 'password',
-            name: 'githubPersonalAccessToken',
-            message: 'Enter your GitHub personal access token:',
+          type: "password",
+          name: "githubPersonalAccessToken",
+          message: "GitHub personal access token: ",
         },
-        {
-            type: 'input',
-            name: 'repositoryName',
-            message: 'Enter your repository name:',
-        },
-        {
-            type: 'input',
-            name: 'projectDirectory',
-            message: 'Enter your project directory:',
-        },
-    ]);
-
-    const { githubUsername, githubPersonalAccessToken, repositoryName, projectDirectory } = answers;
-    let command;
-    if(process.platform == 'linux'){
-        command = `../scripts/shell/index.sh "${githubUsername}" "${githubPersonalAccessToken}" "${repositoryName}" "${projectDirectory}"`;
-    }
-    else{
-        command = `../scripts/batch/index.bat "${githubUsername}" "${githubPersonalAccessToken}" "${repositoryName}" "${projectDirectory}"`;
-    }
-
-    console.log(process.platform)
-
-    exec(command, function (err, stdout, stderr) {
+      ]);
+      writeFile(config_dir, JSON.stringify(answers), (err) => {
         if (err) {
-            console.error('Error executing shell script:', err);
-            return;
-        } else if (stderr) {
-            console.error('Shell script errors:', stderr);
-            return;
+          logError("Error writing to the file:", err);
+        } else {
+          logMessage(chalk.blue.bold("Configurations saved successfully."));
         }
-        console.log(stdout);
-        console.log(boxen('Made with love by <MuhireIghor />', {
-            margin: 1,
-            float: 'left',
-            padding: 1,
-            borderStyle: "single",
-            borderColor: "green"
-        }))
+      });
+    }
+  });
 
-    });
-}
+program
+  .command("new <repository_name>")
+  .description("Create a new repository on Github")
+  .action((repositoryName) => {
+    logMessage(boxen('Made with love by <MuhireIghor />', {
+        margin: 1,
+        float: 'left',
+        padding: 1,
+        borderStyle: "single",
+        borderColor: "green"
+    }))
+  });
 
-main();
+program.parse();
