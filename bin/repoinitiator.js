b586172702c6660761d312c7d6f2cc34ba066354
MURANGWA Pacifique <pacifiquemurangwa001@gmail.com>
2024-02-03 08:58:08 +0200

    feat: handling new repo command

--- bin/repoinitiator.js
+++ bin/repoinitiator.js
@@ -5,10 +5,11 @@ import boxen from "boxen";
 import inquirer from "inquirer";
 import { program } from "commander";
 import pkg from "../package.json" assert { type: "json" };
-import { writeFile, existsSync } from "fs";
+import { writeFile, existsSync, readFileSync } from "fs";
 import chalk from "chalk";
 import { logMessage, logError } from "../lib/log.js";
 import { getConfigDir } from "../lib/config_dir.js";
+import path from "path";
 
 program
   .name("repoinitiator")
@@ -55,14 +56,53 @@ program
 program
   .command("new <repository_name>")
   .description("Create a new repository on Github")
-  .action((repositoryName) => {
-    logMessage(boxen('Made with love by <MuhireIghor />', {
-        margin: 1,
-        float: 'left',
-        padding: 1,
-        borderStyle: "single",
-        borderColor: "green"
-    }))
+  .action(async (repositoryName) => {
+    const config_dir = getConfigDir();
+    if (!existsSync(config_dir)) {
+      logError(
+        "Configurations not found. Please run `repoinitiator config` to configure repoinitiator."
+      );
+      return;
+    }
+    const config_info = JSON.parse(readFileSync(config_dir, "utf8"));
+    const { githubUsername, githubPersonalAccessToken } = config_info;
+    const answers = await inquirer.prompt([
+      {
+        type: "input",
+        name: "directory",
+        message: "Enter the directory for the repository: ",
+      },
+    ]);
+
+    const local_repo_dir = path.join(process.cwd(), answers.directory);
+
+    let command;
+
+    if (process.platform == "linux") {
+      command = `../scripts/shell/index.sh "${githubUsername}" "${githubPersonalAccessToken}" "${repositoryName}" "${local_repo_dir}"`;
+    } else {
+      command = `../scripts/batch/index.bat "${githubUsername}" "${githubPersonalAccessToken}" "${repositoryName}" "${local_repo_dir}"`;
+    }
+
+    exec(command, function (err, stdout, stderr) {
+      if (err) {
+        console.error("Error executing shell script:", err);
+        return;
+      } else if (stderr) {
+        console.error("Shell script errors:", stderr);
+        return;
+      }
+      console.log(stdout);
+      console.log(
+        boxen("Made with love by <MuhireIghor />", {
+          margin: 1,
+          float: "left",
+          padding: 1,
+          borderStyle: "single",
+          borderColor: "green",
+        })
+      );
+    });
   });
 
 program.parse();
